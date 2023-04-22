d3d0dd8eebc701ef1ca2f0d9536912a19ce893a7
Connor Vosberg <connorjv@gmail.com>
2023-04-22 16:35:06 -0600

    commit tree 6cd070663c21b52658a03821ec1e33cdb409aa63

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+require "pry"
+
+def badge_maker (name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (arr_names)
+    arr_names.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms (speakers)
+    speakers.map.with_index(1) do |speaker, index|
+        "Hello, #{speaker}! You'll be assigned to room #{index}!"
+    end
+end
+
+def printer (names_array)
+    batch_badge_creator(names_array).map do |statement| 
+        puts statement
+    end
+    assign_rooms(names_array).map do |statement|
+        puts statement
+    end
+end
